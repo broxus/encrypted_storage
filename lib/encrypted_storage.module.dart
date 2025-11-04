8340c138e7f38f21f41736058ea329868576194c
Egor Komarov <e.komarov.bf@gmail.com>
2025-11-04 17:05:21 +0100

    chore: update workflows and dependencies to use flutter 3.35.6

--- lib/encrypted_storage.module.dart
+++ lib/encrypted_storage.module.dart
@@ -4,15 +4,15 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i2;
+import 'dart:async' as _i687;
 
-import 'package:encrypted_storage/src/encrypted_storage.dart' as _i3;
-import 'package:injectable/injectable.dart' as _i1;
+import 'package:encrypted_storage/src/encrypted_storage.dart' as _i1061;
+import 'package:injectable/injectable.dart' as _i526;
 
-class EncryptedStoragePackageModule extends _i1.MicroPackageModule {
+class EncryptedStoragePackageModule extends _i526.MicroPackageModule {
 // initializes the registration of main-scope dependencies inside of GetIt
   @override
-  _i2.FutureOr<void> init(_i1.GetItHelper gh) {
-    gh.singleton<_i3.EncryptedStorage>(() => _i3.EncryptedStorage());
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+    gh.singleton<_i1061.EncryptedStorage>(() => _i1061.EncryptedStorage());
   }
 }
